0511129183f21a6501df150f2b325f4cf37960c3
Shabdiz <s_ghas02@uni-muenster.de>
2017-10-08 02:38:55 +0200

    - Crawler now shuffles summoners in random order to pick from at the start - added some comments for explainations and edited some console logs - Merge with readme file - Handling for API Key Expiration - Storing already processed Summoners

--- Lib/Crawler.py
+++ Lib/Crawler.py
@@ -36,6 +36,9 @@ class Crawler(threading.Thread):
     def get_matches(self) -> list:
         return self.db.distinct("gameId", {"platformId": self.region})
 
+    def get_fetched_summoners(self) -> list:
+        return self.db.fetchedSummoners.distinct("id", {"platformId": self.region})
+
     def get_summoners(self) -> list:
         return self.db.distinct("participantIdentities.player.currentAccountId", {"platformId": self.region})
 
@@ -51,7 +54,6 @@ class Crawler(threading.Thread):
             }
             }
         ]))}
-        #print(str(self.region) + " Patches in Detail: " + str(detailedPatches))
         patchCycles = {p.patch: 0 for p in PATCH_LIST}
         for k, v in detailedPatches.items():
             patchCycles[re.match("\d.\d+", k, flags=0).group(0)] += v
@@ -64,20 +66,31 @@ class Crawler(threading.Thread):
         fetchedMatches = self.get_matches()
         strdSum = self.get_summoners() # stored summoners in matchDetails collection
         unfetchedSummoners = deque()
-        fetchedSummoners = []
+        fetchedSummoners = self.get_fetched_summoners()
         versions = self.get_versions()
         pprint(versions)
-        unfetchedSummoners.append(
-            self.req.requestSummonerData(self.region, EntrySummoner[self.region].value)["accountId"])
-        for s in random.sample(strdSum, len(strdSum)): unfetchedSummoners.append(s) #random order of summoners
+        if not strdSum: #first run
+            unfetchedSummoners.append(
+                self.req.requestSummonerData(self.region, EntrySummoner[self.region].value)["accountId"])
+        else:
+            for s in list(set(strdSum) - set(fetchedSummoners)): unfetchedSummoners.append(s)
+        print("Number of Summoners in MatchDetails:" +"\t"+ str(len(strdSum)) + "\n" +
+              "Number of Summoners already completely processed:" +"\t" + str(len(fetchedSummoners)) + "\n" +
+              "Number of Summoners to process:" +"\t" + str(len(unfetchedSummoners)))
+        #for s in random.sample(strdSum, len(strdSum)): #random order of summoners
+        #     if s not in fetchedSummoners: unfetchedSummoners.append(s)
+
+
+        # AS LONG AS THERE ARE SUMMONERS TO PROCESS:
         while len(unfetchedSummoners) > 0:
             versions = self.get_versions() #update Versions in case something halfway still went wrong
             summoner = unfetchedSummoners.popleft()
             if summoner in fetchedSummoners:
                 pprint("Summoner: " + str(summoner) + " already processed, continue to next")
                 continue
-
             pprint(self.region + " : Next Summoner ID" + str(summoner))
+
+            #GET MATCHLIST OF SUMMONER
             for attempt in range(1, 10):
                 try:
                     currentMatchlist = req.requestMatchList(self.region, summoner, queue=420)
@@ -90,6 +103,8 @@ class Crawler(threading.Thread):
             else:
                 pprint("Exceeded 10 attempts. Going to next Summoner")
                 continue
+
+            # ITERATE THROUGH ALL HIS RANKED GAMES IN SEASON 7
             for match in currentMatchlist:
                 patch = getPatchFromTimestamp(match['timestamp'])
                 if match['gameId'] not in fetchedMatches and patch in versions.keys() and versions[patch] < self.matchesPerPatch and self.region == match['platformId']:
@@ -98,6 +113,10 @@ class Crawler(threading.Thread):
                             details = req.requestMatchData(self.region, match['gameId'])
                             details["timeline"] = req.requestMatchTimelineData(self.region, match['gameId'])
                             break
+                        except ForbiddenException as e:
+                            pprint(self.region + " {} :".format(
+                                datetime.datetime.now() - self.start_time) + e.message + ": Change up your API Key! Script will abort for now" )
+                            return
                         except Exception as e:
                             pprint(self.region + " {} :".format(
                                 datetime.datetime.now() - self.start_time) + e.message + " when trying to fetch Match " + str(
@@ -114,6 +133,9 @@ class Crawler(threading.Thread):
                     versions[patch] += 1
                     self.db.insert_one(details)
                     print(self.region+": "+ str(versions))
+
+            #after processing summmoner:
             fetchedSummoners.append(summoner)
-            ("All games from Summoner: " + str(summoner) + " processd! Continue to next")
+            blub = self.db.fetchedSummoners.insert_one({"id": summoner, "platformId": self.region})
+            print("Completed processing Summoner " +str(summoner)+ "! Stored him into the MongoDB collection with the _id: " + str(blub.inserted_id))
             if all(v >= self.matchesPerPatch for v in versions.values()): break
--- Lib/patches.py
+++ Lib/patches.py
@@ -11,28 +11,28 @@ class Patch:
         self.start = start
         self.end = end
 
-PATCH_LIST = [
-    Patch(name="Season 6", patch="6.22", start=datetime.date(2016, 11, 10), end=datetime.date(2016, 11, 22)),
-    Patch(name="Season 6", patch="6.23", start=datetime.date(2016, 11, 22), end=datetime.date(2016, 12, 7)),
-    Patch(name="Season 6", patch="6.24", start=datetime.date(2016, 12, 7), end=datetime.date(2017, 1, 11)),
-    Patch(name="Season 7", patch="7.1", start=datetime.date(2017, 1, 11), end=datetime.date(2017, 1, 25)),
-    Patch(name="Season 7", patch="7.2", start=datetime.date(2017, 1, 25), end=datetime.date(2017, 2, 8)),
-    Patch(name="Season 7", patch="7.3", start=datetime.date(2017, 2, 8), end=datetime.date(2017, 2, 23)),
-    Patch(name="Season 7", patch="7.4", start=datetime.date(2017, 2, 23), end=datetime.date(2017, 3, 8)),
-    Patch(name="Season 7", patch="7.5", start=datetime.date(2017, 3, 8), end=datetime.date(2017, 3, 22)),
-    Patch(name="Season 7", patch="7.6", start=datetime.date(2017, 3, 22), end=datetime.date(2017, 4, 5)),
-    Patch(name="Season 7", patch="7.7", start=datetime.date(2017, 4, 5), end=datetime.date(2017, 4, 19)),
-    Patch(name="Season 7", patch="7.8", start=datetime.date(2017, 4, 19), end=datetime.date(2017, 5, 3)),
-    Patch(name="Season 7", patch="7.9", start=datetime.date(2017, 5, 3), end=datetime.date(2017, 5, 17)),
-    Patch(name="Season 7", patch="7.10", start=datetime.date(2017, 5, 17), end=datetime.date(2017, 6, 1)),
-    Patch(name="Season 7", patch="7.11", start=datetime.date(2017, 6, 1), end=datetime.date(2017, 6, 14)),
-    Patch(name="Season 7", patch="7.12", start=datetime.date(2017, 6, 14), end=datetime.date(2017, 6, 28)),
-    Patch(name="Season 7", patch="7.13", start=datetime.date(2017, 6, 28), end=datetime.date(2017, 7, 12)),
-    Patch(name="Season 7", patch="7.14", start=datetime.date(2017, 7, 12), end=datetime.date(2017, 7, 26)),
-    Patch(name="Season 7", patch="7.15", start=datetime.date(2017, 7, 26), end=datetime.date(2017, 8, 9)),
-    Patch(name="Season 7", patch="7.16", start=datetime.date(2017, 8, 9), end=datetime.date(2017, 8, 23)),
-    Patch(name="Season 7", patch="7.17", start=datetime.date(2017, 8, 23), end=datetime.date(2017, 9, 13)),
-    Patch(name="Season 7", patch="7.18", start=datetime.date(2017, 9, 13), end=datetime.date(2017, 9, 27))
+PATCH_LIST = [ # changed the dates to circumvent different patch deployment dates throughout all regions as much as possible
+    Patch(name="Season 6", patch="6.22", start=datetime.date(2016, 11, 10), end=datetime.date(2016, 11, 21)), # start=datetime.date(2016, 11, 10), end=datetime.date(2016, 11, 22)
+    Patch(name="Season 6", patch="6.23", start=datetime.date(2016, 11, 23), end=datetime.date(2016, 12, 6)), # start=datetime.date(2016, 11, 22), end=datetime.date(2016, 12, 7)
+    Patch(name="Season 6", patch="6.24", start=datetime.date(2016, 12, 8), end=datetime.date(2017, 1, 10)), # start=datetime.date(2016, 12, 7), end=datetime.date(2017, 1, 11)
+    Patch(name="Season 7", patch="7.1", start=datetime.date(2017, 1, 12), end=datetime.date(2017, 1, 24)), # start=datetime.date(2017, 1, 11), end=datetime.date(2017, 1, 25)
+    Patch(name="Season 7", patch="7.2", start=datetime.date(2017, 1, 26), end=datetime.date(2017, 2, 7)), # start=datetime.date(2017, 1, 25), end=datetime.date(2017, 2, 8)
+    Patch(name="Season 7", patch="7.3", start=datetime.date(2017, 2, 9), end=datetime.date(2017, 2, 22)), # start=datetime.date(2017, 2, 8), end=datetime.date(2017, 2, 23)
+    Patch(name="Season 7", patch="7.4", start=datetime.date(2017, 2, 24), end=datetime.date(2017, 3, 7)), # start=datetime.date(2017, 2, 23), end=datetime.date(2017, 3, 8)
+    Patch(name="Season 7", patch="7.5", start=datetime.date(2017, 3, 9), end=datetime.date(2017, 3, 21)), # start=datetime.date(2017, 3, 8), end=datetime.date(2017, 3, 22)
+    Patch(name="Season 7", patch="7.6", start=datetime.date(2017, 3, 23), end=datetime.date(2017, 4, 4)), #start=datetime.date(2017, 3, 22), end=datetime.date(2017, 4, 5))
+    Patch(name="Season 7", patch="7.7", start=datetime.date(2017, 4, 6), end=datetime.date(2017, 4, 18)), # start=datetime.date(2017, 4, 5), end=datetime.date(2017, 4, 19)
+    Patch(name="Season 7", patch="7.8", start=datetime.date(2017, 4, 20), end=datetime.date(2017, 5, 2)), # start=datetime.date(2017, 4, 19), end=datetime.date(2017, 5, 3)
+    Patch(name="Season 7", patch="7.9", start=datetime.date(2017, 5, 4), end=datetime.date(2017, 5, 16)), #start=datetime.date(2017, 5, 3), end=datetime.date(2017, 5, 17)
+    Patch(name="Season 7", patch="7.10", start=datetime.date(2017, 5, 18), end=datetime.date(2017, 5, 30)), # start=datetime.date(2017, 5, 17), end=datetime.date(2017, 6, 1)
+    Patch(name="Season 7", patch="7.11", start=datetime.date(2017, 6, 2), end=datetime.date(2017, 6, 13)), # start=datetime.date(2017, 6, 1), end=datetime.date(2017, 6, 14)
+    Patch(name="Season 7", patch="7.12", start=datetime.date(2017, 6, 15), end=datetime.date(2017, 6, 27)), # start=datetime.date(2017, 6, 14), end=datetime.date(2017, 6, 28)
+    Patch(name="Season 7", patch="7.13", start=datetime.date(2017, 6, 29), end=datetime.date(2017, 7, 11)),# start=datetime.date(2017, 6, 28), end=datetime.date(2017, 7, 12)
+    Patch(name="Season 7", patch="7.14", start=datetime.date(2017, 7, 13), end=datetime.date(2017, 7, 25)),# start=datetime.date(2017, 7, 12), end=datetime.date(2017, 7, 26)
+    Patch(name="Season 7", patch="7.15", start=datetime.date(2017, 7, 27), end=datetime.date(2017, 8, 8)), # start=datetime.date(2017, 7, 26), end=datetime.date(2017, 8, 9)
+    Patch(name="Season 7", patch="7.16", start=datetime.date(2017, 8, 10), end=datetime.date(2017, 8, 22)),# start=datetime.date(2017, 8, 9), end=datetime.date(2017, 8, 23)
+    Patch(name="Season 7", patch="7.17", start=datetime.date(2017, 8, 24), end=datetime.date(2017, 9, 12)), #start=datetime.date(2017, 8, 23), end=datetime.date(2017, 9, 13)
+    Patch(name="Season 7", patch="7.18", start=datetime.date(2017, 9, 14), end=datetime.date(2017, 9, 26)) # start=datetime.date(2017, 9, 13), end=datetime.date(2017, 9, 27)
 ]
 
 
