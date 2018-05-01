4f11c0f8255ff762a9354e3f4d5e4f6d65b4bf3b
Shabdiz <s_ghas02@uni-muenster.de>
2018-05-01 17:11:44 +0200

    - getting ready for "prepare_purchases"

--- prepare_purchases.py
+++ prepare_purchases.py
@@ -8,18 +8,18 @@ def main():
     mongoDB = MongoClient('mongodb://localhost:27017/').LeagueCrawler
     globalStart, start = datetime.now(), datetime.now()
 
-    # # events:
-    # mongoDB.matchDetails.aggregate(pl.get_adc_events(), allowDiskUse=True)
-    # print("Events finished after: " + str(datetime.now() - start))
-    #
-    # # frames:
-    # start = datetime.now()
-    # print("Start Frames after: " + str(datetime.now() - start))
-    # mongoDB.matchDetails.aggregate(pl.get_frames(sample), allowDiskUse=True)
-    # print("Frames finished after: " + str(datetime.now() - start))
-    # mongoDB.adc_frames.create_index([('championId', ASCENDING)])
-    # mongoDB.adc_frames.create_index([('championId', ASCENDING),
-    #                                  ('patch', ASCENDING)])
+    # events:
+    mongoDB.matchDetails.aggregate(pl.get_adc_events(), allowDiskUse=True)
+    print("Events finished after: " + str(datetime.now() - start))
+
+    # frames:
+    start = datetime.now()
+    print("Start Frames after: " + str(datetime.now() - start))
+    mongoDB.matchDetails.aggregate(pl.get_frames(), allowDiskUse=True)
+    print("Frames finished after: " + str(datetime.now() - start))
+    mongoDB.adc_frames.create_index([('championId', ASCENDING)])
+    mongoDB.adc_frames.create_index([('championId', ASCENDING),
+                                     ('patch', ASCENDING)])
 
     # purchase details:
     start = datetime.now()
@@ -32,19 +32,19 @@ def main():
     mongoDB.adc_purchase_details.create_index([("championId", ASCENDING), ("patch", ASCENDING)])
     print("Script finished after: " + str(datetime.now() - globalStart))
 
-    # mongoDB.adc_events.create_index([("championId", ASCENDING),
-    #                                  ("gameId", ASCENDING),
-    #                                  ("timestamp", ASCENDING)
-    #                                  ])
-    # start = datetime.now()
-    # print("Indexing Events finished after: " + str(datetime.now() - start))
-    #
-    # mongoDB.adc_events.create_index([("championId", ASCENDING),
-    #                                  ("gameId", ASCENDING),
-    #                                  ("frameNo", ASCENDING)
-    #                                  ])
-    # start = datetime.now()
-    # print("Indexing Frames finished after: " + str(datetime.now() - start))
+    mongoDB.adc_events.create_index([("championId", ASCENDING),
+                                     ("gameId", ASCENDING),
+                                     ("timestamp", ASCENDING)
+                                     ])
+    start = datetime.now()
+    print("Indexing Events finished after: " + str(datetime.now() - start))
+
+    mongoDB.adc_events.create_index([("championId", ASCENDING),
+                                     ("gameId", ASCENDING),
+                                     ("frameNo", ASCENDING)
+                                     ])
+    start = datetime.now()
+    print("Indexing Frames finished after: " + str(datetime.now() - start))
 
     print("Start Counting Items at: ", str(datetime.now()))
     mongoDB.adc_purchase_details.aggregate(pl.get_item_count(), allowDiskUse=True)
